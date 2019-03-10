f61e0bbfb5a9de7efb65035101d505f50fec45c9
David Gitz <davidgitz@gmail.com>
2019-03-09 19:08:50 -0600

    Bucket complete.  Going to update plugin next with basic subs and that will finish this US.

---
+++ src/robot_model.h
@@ -0,0 +1,15 @@
+/*
+ * robot_model.h
+ *
+ *  Created on: Mar 9, 2019
+ *      Author: robot
+ */
+
+#ifndef SRC_ICARUS_SIM_SRC_ROBOT_MODEL_H_
+#define SRC_ICARUS_SIM_SRC_ROBOT_MODEL_H_
+
+
+
+
+
+#endif /* SRC_ICARUS_SIM_SRC_ROBOT_MODEL_H_ */
